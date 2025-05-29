b54a1b0880e5f4c8ddf409f25a43316f882e2e20
victor santos <victor.santos@lexartlabs.xyz>
2025-05-29 11:43:26 -0300

    fix: Fixed bulk function

--- backend/src/variations/index.js
+++ backend/src/variations/index.js
@@ -3,8 +3,9 @@ const router = require('express').Router();
 
 router.get('/', express.json(), async (req, res, next) => {
   try {
-    const localVariations = require('../../output/variacoes.json')
+    let localVariations = require('../../output/variacoes.json')
     const variations = localVariations.map(item => String(item).toLowerCase());
+    console.log(variations, "variations");
     return res.json(variations);
   } catch (error) {
     return next(error)
