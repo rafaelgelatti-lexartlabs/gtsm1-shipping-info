4631b9ceaa35b712684891376d304e6ebe068cf4
victor santos <victor.santos@lexartlabs.xyz>
2025-05-29 11:53:16 -0300

    fix: Fixed require caching variations.json

--- backend/src/variations/index.js
+++ backend/src/variations/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = require('express').Router();
+const fs = require('fs').promises;
 
 router.get('/', express.json(), async (req, res, next) => {
   try {
-    let localVariations = require('../output/variacoes.json')
+    const filePath = path.join(__dirname, '..', 'output', 'variacoes.json');
+    const fileData = await fs.readFile(filePath, 'utf8');
+    const localVariations = JSON.parse(fileData);
     const variations = localVariations.map(item => String(item).toLowerCase());
     console.log(variations, "variations");
     return res.json(variations);
